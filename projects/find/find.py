057fe5419d1f74687751f84de031b9c00027ddd9
Gianluca Parente <gianluca.parente7@gmail.com>
2016-05-26 21:11:09 +0100

    argparse e clean code DONE

--- projects/find/find.py
+++ projects/find/find.py
@@ -1,12 +1,11 @@
 import os
-import sys
+import argparse
 
 """This method is created to find all files and directories, given a specific path. Optionally, your can use -name flag
 to search only the files that match to a specific name"""
 
 
-def find(path, option=None, filename=None):
-    """this method uses a bfs approach to resolve to the problem of finding all files and directories."""
+def find(path, name=None):
     if not os.path.exists(path):
         return "Path doesn't exist or wrong: please try again"
     queue = []
@@ -17,22 +16,19 @@ def find(path, option=None, filename=None):
         adj_list = os.listdir(queue_element)
         if len(adj_list) != 0:
             for adj in adj_list:
-                if filename is not None and option == "-name":
-                    if os.path.isfile(os.path.join(queue_element, adj)) and adj == filename:
-                        paths_list.append(os.path.join(queue_element, adj))
-                    elif os.path.isdir(os.path.join(queue_element, adj)):
-                        queue.append(os.path.join(queue_element, adj))
-                elif filename is None and option is None:
-                    if os.path.isfile(os.path.join(queue_element, adj)):
-                        paths_list.append(os.path.join(queue_element, adj))
-                    elif os.path.isdir(os.path.join(queue_element, adj)):
-                        queue.append(os.path.join(queue_element, adj))
-                        paths_list.append(os.path.join(queue_element, adj))
+                if os.path.isdir(os.path.join(queue_element, adj)):
+                    queue.append(os.path.join(queue_element, adj))
+
+                if not name or (name is not None and adj.startswith(name)):
+                    paths_list.append(os.path.join(queue_element, adj))
+
     paths_list.sort()
     for path in paths_list:
         print path
 
 if __name__ == '__main__':
-    find(str(sys.argv[1]), option=str(sys.argv[2]), filename=str(sys.argv[3]))
-    # print find("/home/gianluca/projects/exercises/", None, None)
-    # print find("/home/gianluca/projects/exercises/", "-name", "tree.py")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory')
+    parser.add_argument('-name', type=str, default=None)
+    args = parser.parse_args()
+    find(args.directory, args.name)
