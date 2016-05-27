151c896ccb87e704d889319d192fd0e887c14b91
Gianluca Parente <gianluca.parente7@gmail.com>
2016-05-27 19:20:13 +0100

    Symlink DONE BUT not testedgit status! Wait the tests before analyze it!

--- projects/find/find.py
+++ projects/find/find.py
@@ -12,14 +12,17 @@ def find(path, name=None):
     stack.append(path)
     while len(stack) > 0:
         stack_element = stack.pop(0)
-        if os.path.isdir(stack_element):
-            adj_list = os.listdir(stack_element)
-            adj_list.sort(reverse=True)
-            for adj in adj_list:
-                stack.insert(0, os.path.join(stack_element, adj))
+        if not os.path.islink(stack_element):
+            if os.path.isdir(stack_element):
+                adj_list = os.listdir(stack_element)
+                adj_list.sort(reverse=True)
+                for adj in adj_list:
+                    stack.insert(0, os.path.join(stack_element, adj))
 
-        last_suffix_stack_el = stack_element.rsplit("/", 1)[1]
-        if not name or (name is not None and last_suffix_stack_el.startswith(name)):
+            last_suffix_stack_el = stack_element.rsplit("/", 1)[1]
+            if not name or (name is not None and last_suffix_stack_el.startswith(name)):
+                print stack_element
+        else:
             print stack_element
 
 if __name__ == '__main__':
@@ -28,3 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('-name', type=str, default=None)
     args = parser.parse_args()
     find(args.directory, args.name)
+    find("/home/gianluca/Downloads")
+    # os.symlink("/home/gianluca/Downloads", "/home/gianluca/Downloads/test/subtest/try")
+    # print os.readlink("/home/gianluca/Downloads/test/subtest/try")
+    # print str(os.path.islink("/home/gianluca/Downloads/test/subtest/try"))
