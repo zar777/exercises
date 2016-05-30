00fb853aca3788643e56639f657065164fc308b8
Gianluca Parente <gianluca.parente7@gmail.com>
2016-05-30 13:14:49 +0100

    Modified DONE

--- projects/find/find.py
+++ projects/find/find.py
@@ -2,26 +2,23 @@ import os
 import sys
 import argparse
 
-"""This method is created to find all files and directories, given a specific path. Optionally, your can use -name flag
-to search only the files that match to a specific name"""
-
 
 def find(path, name=None, out=sys.stdout):
+    """This method is created to find all files and directories, given a specific path.
+    Optionally, your can use -name flag to search only the files that match to a specific name"""
     if not os.path.exists(path):
         raise ValueError("Path doesn't exist or wrong: please try again")
     stack = [path]
     while len(stack) > 0:
-        stack_element = stack.pop(0)
+        element = stack.pop()
         # decided to follow the same implementation of real find in which symlink are not navigate through
-        if os.path.isdir(stack_element) and not os.path.islink(stack_element):
-            adj_list = os.listdir(stack_element)
-            adj_list.sort()
-            for adj in adj_list:
-                stack.append(os.path.join(stack_element, adj))
+        if os.path.isdir(element) and not os.path.islink(element):
+            for adj in sorted(os.listdir(element), reverse=True):
+                stack.append(os.path.join(element, adj))
 
-        _, filename = os.path.split(stack_element)
+        _, filename = os.path.split(element)
         if not name or filename.startswith(name):
-            out.write('%s\n' % stack_element)
+            out.write('%s\n' % element)
 
 
 if __name__ == '__main__':
