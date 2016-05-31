b128e77dc4abed85604ca16b378f6b2c91e341d1
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-01 00:43:18 +0100

    Modified Done, Follow up DONE

--- projects/find/find.py
+++ projects/find/find.py
@@ -3,22 +3,32 @@ import sys
 import argparse
 
 
+def _print_path(path, out):
+    if os.path.islink(path):
+        out.write('%s' % path + '->' + os.path.realpath(path) + '\n')
+    else:
+        out.write('%s\n' % path)
+
+
 def find(path, name=None, out=sys.stdout):
     """This method is created to find all files and directories, given a specific path.
     Optionally, your can use -name flag to search only the files that match to a specific name"""
     if not os.path.exists(path):
         raise ValueError("Path doesn't exist or wrong: please try again")
+    marked = set()
     stack = [path]
     while len(stack) > 0:
-        element = stack.pop()
-        # decided to follow the same implementation of real find in which symlink are not navigate through
-        if os.path.isdir(element) and not os.path.islink(element):
-            for adj in sorted(os.listdir(element), reverse=True):
-                stack.append(os.path.join(element, adj))
+        curr_path = stack.pop()
+        real_path = os.path.realpath(curr_path) if os.path.islink(curr_path) else curr_path
+        if real_path not in marked:
+            marked.add(curr_path)
+            if os.path.isdir(real_path):
+                for adj in sorted(os.listdir(real_path), reverse=True):
+                    stack.append(os.path.join(real_path, adj))
 
-        _, filename = os.path.split(element)
-        if not name or filename.startswith(name):
-            out.write('%s\n' % element)
+        # print only if name is not defined or name is in the current path
+        if not name or name in curr_path:
+            _print_path(curr_path, out)
 
 
 if __name__ == '__main__':
