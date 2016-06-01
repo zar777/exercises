46ee332ef425cecdbca1bcd74451fa94e34b1788
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-02 00:52:11 +0100

    File cleaned!

---
+++ clean_project/clean_file.py
@@ -0,0 +1,16 @@
+import string
+
+
+def clean_up(src_path, dest_path):
+    with open(src_path) as source_file:
+        with open(dest_path, "w") as dest_file:
+            for line in source_file:
+                table = string.maketrans(line, line)
+                new_line = line.translate(table, string.punctuation)
+                dest_file.write(new_line)
+            source_file.close()
+            dest_file.close()
+
+
+if __name__ == '__main__':
+    clean_up('/home/gianluca/Desktop/news', '/home/gianluca/Desktop/write_file')
