0976639e148f17c7d4ee66c2abd9e0ade5bdce8a
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-02 16:37:33 +0100

    clean method done but not TESTED... wait the tests

--- clean_project/clean_file.py
+++ clean_project/clean_file.py
@@ -1,15 +1,21 @@
-import string
+import unicodedata
+import sys
+
+table = dict.fromkeys(i for i in xrange(sys.maxunicode)
+                      if unicodedata.category(unichr(i)).startswith('P'))
+
+
+def remove_punctuations(line):
+    a = line.decode('utf-8').translate(table)
+    return a
 
 
 def clean_up(src_path, dest_path):
     with open(src_path) as source_file:
         with open(dest_path, "w") as dest_file:
             for line in source_file:
-                table = string.maketrans(line, line)
-                new_line = line.translate(table, string.punctuation)
-                dest_file.write(new_line)
+                dest_file.write(remove_punctuations(line).encode('utf-8'))
 
 
 if __name__ == '__main__':
-    # Example used to  clean up the example file
     clean_up('/home/gianluca/Desktop/news', '/home/gianluca/Desktop/write_file')
