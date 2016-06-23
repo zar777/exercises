6513d566892c429cc5b8dad060ab15c71f8f93ed
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-23 18:54:37 +0100

    search test done

--- clean_project/search_test.py
+++ clean_project/search_test.py
@@ -1,19 +1,30 @@
+import json
+import tempfile
 import unittest
 
+import index
 import search
 
 
 class SearchTest(unittest.TestCase):
 
     def test_no_match(self):
-        self.assertEqual("No matches", search.search("fake_word", ))
+        with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
+            index.index('test_data/config_file.yaml', index_file.name)
+            self.assertEqual([], search.search("wrongword", index_file.name))
 
     def test_match(self):
-        self.assertEqual([1, 3], search.search("bulgaria", ))
+        with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
+            index.index('test_data/config_file.yaml', index_file.name)
+            self.assertEqual([(u'test_data/full_file_dirty.txt', [1, 3])],
+                             search.search("bulgaria", index_file.name))
 
     def test_no_match_single_character(self):
-        self.assertEqual("No matches", search.search("s", ))
+        with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
+            index.index('test_data/config_file.yaml', index_file.name)
+            self.assertEqual([], search.search("a", index_file.name))
 
     def test_no_match_punctuation(self):
-        self.assertEqual("No matches", search.search("'", ))
-
+        with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
+            index.index('test_data/config_file.yaml', index_file.name)
+            self.assertEqual([], search.search("-more_video's:!", index_file.name))
