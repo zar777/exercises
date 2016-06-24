61ba9472cc452b60fd7ae95f8fb73bd4caad9fae
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-24 11:38:16 +0100

    Code review done

--- clean_project/index_test.py
+++ clean_project/index_test.py
@@ -3,22 +3,35 @@ import tempfile
 import unittest
 
 import index
+import collections
 
 
-class ConstructDictTest(unittest.TestCase):
+def convert(data):
+    if isinstance(data, basestring):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
+
+
+class IndexTest(unittest.TestCase):
 
     def test_empty_yaml_file(self):
         with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
-            self.assertEqual("Empty config file", index.index('test_data/config_empty_file.yaml', index_file.name))
+            self.assertRaises(ValueError, index.index, 'test_data/config_empty_file.yaml', index_file.name)
 
     def test_full_dictionary(self):
         with tempfile.NamedTemporaryFile(suffix="JSON") as index_file:
             index.index('test_data/config_file.yaml', index_file.name)
             result = json.load(index_file)
-            self.assertEqual({u'the': {u'test_data/full_file_dirty.txt': [1]},
-                              u'as': {u'test_data/full_file_dirty.txt': [1, 2]},
-                              u'bush': {u'test_data/full_file_dirty.txt': [1]},
-                              u'bulgaria': {u'test_data/full_file_dirty.txt': [1, 3]}}, result)
+            result_converted = convert(result)
+            self.assertEqual({'the': {'test_data/full_file_dirty.txt': [1]},
+                              'bush': {'test_data/full_file_dirty.txt': [1]},
+                              'as': {'test_data/full_file_dirty.txt': [1, 2]},
+                              'bulgaria': {'test_data/full_file_dirty.txt': [1, 3]}}, result_converted)
 
     def test_file_not_exist(self):
         self.assertRaises(IOError, index.index, "/path/file_not_exist", '/path/index_file.json')
