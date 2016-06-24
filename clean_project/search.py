2bc4d1046f9ef449c3c8a1577b80ba507c860a32
Gianluca Parente <gianluca.parente7@gmail.com>
2016-06-24 17:23:30 +0100

    Code review done

--- clean_project/search.py
+++ clean_project/search.py
@@ -8,34 +8,36 @@ BOLD = '\033[1m'
 END = '\033[0m'
 
 
-def search(search_word, json_path):
-    """
-    Search if a given word is in bucket of files
-    :param search_word: Word given to search
-    :param json_path: Indexing file
-    :return: List of files and numbers of file when search_word occurrence
-    """
-    with open(json_path) as json_file:
-        index = json.load(json_file)
+class Search(object):
+    def __init__(self, json_path):
+        with open(json_path) as json_file:
+            index = json.load(json_file)
+        self.index = index
+
+    def search(self, search_word):
+        """
+        Search if a given word is in bucket of files
+        :param search_word: Word given to search
+        :return: List of files and numbers of file when search_word occurrence
+        """
         result_list = []
-        if search_word in index:
-            result_list = index[search_word].items()
-    return result_list
+        if search_word in self.index:
+            result_list = self.index[search_word].items()
+        return result_list
 
-
-def print_output(search_word, results):
-    """
-    Print output of search
-    :param search_word: Word given to search
-    :param results: list of tuples(filename, occurrence in a given file)
-    """
-    print "The word {bold} {word} {end} is contained in the following file/lines:" \
-        .format(bold=BOLD, word=search_word, end=END)
-    if results:
-        for filename, occurrence in results:
-            print "--> %s: %s" % (filename, str(occurrence).strip("[]"))
-    else:
-        print "No matches"
+    def print_output(self, search_word, results):
+        """
+        Print output of search
+        :param search_word: Word given to search
+        :param results: list of tuples(filename, occurrence in a given file)
+        """
+        if results:
+            print "The word {bold} {word} {end} is contained in the following file/lines:" \
+                .format(bold=BOLD, word=search_word, end=END)
+            for filename, occurrence in results:
+                    print "--> %s: %s" % (filename, str(occurrence).strip("[]"))
+        else:
+            print "No matches"
 
 
 if __name__ == '__main__':
@@ -44,8 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('json_path')
     args = parser.parse_args()
     try:
-        results = search(args.word, args.json_path)
-        print results
-        print_output(args.word, results)
+        search_class = Search(args.json_path)
+        results = search_class.search(args.word)
+        search_class.print_output(args.word, results)
     except IOError as e:
         print 'File or path not found: %s' % e
--- clean_project/search_cli.py
+++ clean_project/search_cli.py
@@ -4,17 +4,17 @@ Search a word in a file and return all the occurrence specified the line numbers
 import argparse
 import cmd
 
-import search
+from search import Search
 
 BOLD = '\033[1m'
 END = '\033[0m'
 
 
-class Search(cmd.Cmd):
+class SearchCli(cmd.Cmd):
 
-    def __init__(self, json_path):
+    def __init__(self, search):
         cmd.Cmd.__init__(self)
-        self.json_path = json_path
+        self.search = search
 
     def do_search(self, search_word):
         """
@@ -22,8 +22,8 @@ class Search(cmd.Cmd):
         :param search_word: Word given to search
         """
         try:
-            results = search.search(search_word, self.json_path)
-            search.print_output(search_word, results)
+            results = search_class.search(search_word)
+            search_class.print_output(search_word, results)
         except IOError as e:
             print 'File or path not found: %s' % e
 
@@ -32,5 +32,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('json_path')
     args = parser.parse_args()
-    cmd_line = Search(args.json_path)
+    search_class = Search(args.json_path)
+    cmd_line = SearchCli(search_class)
     cmd_line.cmdloop()
