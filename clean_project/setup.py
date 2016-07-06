fc734ad639a60f928b266cb1905fcd1520a29d34
Gianluca Parente <gianluca.parente7@gmail.com>
2016-07-06 16:39:38 +0100

    license and setup added

---
+++ clean_project/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="clean_project",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Gianluca Parente",
+    author_email="gianluca.parente7@gmail.com",
+
+    # Packages
+    packages=["project"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="",
+
+    #
+    # license="LICENSE.txt",
+    description="",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "flask",
+        "psycopg2",
+        "pyyaml",
+    ],
+)
