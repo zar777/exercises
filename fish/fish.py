90b856bd81392433dc004bb801aa7e97df8540ad
Gianluca Parente <gianluca.parente7@gmail.com>
2016-07-22 16:51:12 +0100

    Failed

---
+++ fish/fish.py
@@ -0,0 +1,12 @@
+# you can write to stdout for debugging purposes, e.g.
+# print "this is a debug message"
+
+def solution(A, B):
+    sol = []
+    while 
+        if B[]
+
+    return len(sol)
+
+
+if __name__ == '__main__':
