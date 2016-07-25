3620779bc709644d603958c256c4bbde60770834
Gianluca Parente <gianluca.parente7@gmail.com>
2016-07-25 09:52:28 +0100

    Discussed with Riccardo

---
+++ perimeter/perimeter.py
@@ -0,0 +1,42 @@
+# An integer N is given, representing the area of some rectangle.
+#
+# The area of a rectangle whose sides are of length A and B is A * B, and the perimeter is 2 * (A + B).
+#
+# The goal is to find the minimal perimeter of any rectangle whose area equals N. The sides of this rectangle should be only integers.
+#
+# For example, given integer N = 30, rectangles of area 30 are:
+#
+# (1, 30), with a perimeter of 62,
+# (2, 15), with a perimeter of 34,
+# (3, 10), with a perimeter of 26,
+# (5, 6), with a perimeter of 22.
+# Write a function:
+#
+# def solution(N)
+#
+# that, given an integer N, returns the minimal perimeter of any rectangle whose area is exactly equal to N.
+#
+# For example, given an integer N = 30, the function should return 22, as explained above.
+#
+# Assume that:
+#
+# N is an integer within the range [1..1,000,000,000].
+# Complexity:
+#
+# expected worst-case time complexity is O(sqrt(N));
+# expected worst-case space complexity is O(1).
+import math
+
+
+def solution(N):
+    if N % 2 != 0:
+        return 2 * (N + 1)
+    side_one = int(round(math.sqrt(N), 1))
+    side_two = int(round(N / side_one, 1))
+
+    return 2 * (side_one + side_two)
+
+
+if __name__ == '__main__':
+    N = 1234
+    print solution(N)
