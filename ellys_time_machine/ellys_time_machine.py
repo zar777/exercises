c57b8bca96d5139db9d19a69ac4810d5f77d69cc
Gianluca Parente <gianluca.parente7@gmail.com>
2016-08-07 13:11:32 +0100

    DONE

--- ellys_time_machine/ellys_time_machine.py
+++ ellys_time_machine/ellys_time_machine.py
@@ -1,10 +1,17 @@
 class EllysTimeMachine(object):
     def getTime(self, time):
-        s = ("12" if time[3:] == "00" else str(int(time[3:])/5).zfill(2)) + "%s" + str(int(time[:2])*5).zfill(2)
+        minutes = int(time[:2])*5 if int(time[:2])*5 != 60 else 00
+        hours = (12 if time[3:] == "00" else int(time[3:])/5)
+        if int(time[:2])*5 == 60:
+            if hours == 12:
+                hours = 1
+            else:
+                hours += 1
+        s = str(hours).zfill(2) + "%s" + str(minutes).zfill(2)
         conversion = s % ":"
         return conversion
 
 if __name__ == '__main__':
-    time = "01:00"
+    time = "12:25"
     elly = EllysTimeMachine()
     print elly.getTime(time)
