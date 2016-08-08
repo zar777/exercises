9abed1afa01a0b5312ceae1554aa704f3aff9413
Gianluca Parente <gianluca.parente7@gmail.com>
2016-08-08 13:20:57 +0100

    -

--- firing_employees/firing_employees.py
+++ firing_employees/firing_employees.py
@@ -12,17 +12,16 @@ class FiringEmployees(object):
                     profit_partial = 0
                 if manager[index] == manager[index + 1]:
                     profit_partial = 0
-                    if not (index < len(manager)-3 and manager[index+1] < manager[index + 2]):
-                       break
             else:
-                if salary_emplo > 0:
+                profit_partial += salary_emplo
+                if profit_partial > 0:
                     profit += salary_emplo
         return profit
 
 
 if __name__ == '__main__':
-    m = [0,0,1,1,2,2]
-    s = [1,1,1,2,2,2]
-    p = [2,2,2,1,1,1]
+    m = [0,0,0]
+    s = [1,2,3]
+    p = [3,2,1]
     firing = FiringEmployees()
     print firing.fire(m,s,p)
