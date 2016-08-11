4788c6d416c46c6d6588c2ae93be403dd0635c26
Gianluca Parente <gianluca.parente7@gmail.com>
2016-08-11 11:16:15 +0100

    To Do

--- MultiplyXPlusOne/MultiplyXPlusOne.py
+++ MultiplyXPlusOne/MultiplyXPlusOne.py
@@ -13,15 +13,15 @@
 #         return solution(s, t / 2, count+1)
 
 def solution(s, t, count):
-    while
         if s > t:
             return -1
         if t == s:
             return count
         t -= 1
         if t % 3 == 0 and t % 2 == 0:
-            solution(s, t / 3, count + 1)
-            solution(s, t / 2, count + 1)
+            a = solution(s, t / 3, count + 1)
+            b = solution(s, t / 2, count + 1)
+            return min(a, b)
         if t % 3 == 0:
             return solution(s, t/3, count+1)
         elif t % 2 == 0:
@@ -29,4 +29,4 @@ def solution(s, t, count):
 
 
 if __name__ == '__main__':
-    print solution(1, 31, 0)
+    print solution(1, 22, 0)
